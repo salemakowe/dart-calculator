e6674f7fbda0bae00ca22e6c6e2ae4279c8e5fda
Salem Akowe <salemakowe@gmail.com>
2022-10-11 11:38:41 +0100

    .

---
+++ stars.dart
@@ -0,0 +1,21 @@
+import "dart:io";
+import "dart:math";
+
+void main() {
+  int t1, t2;
+
+  stdout.writeln("Welcome to Stars");
+  stdout.writeln("What's the lenght of your star table? ");
+  t1 = int.parse(stdin.readLineSync()!);
+
+  // stdout.writeln("Whats the width of your star table? ");
+  // t2 = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= t1; i++) {
+    for (int j = 1; j < i; j++) {
+      stdout.write(j);
+    }
+    // stdout.writeln(i);
+  }
+  ;
+}
